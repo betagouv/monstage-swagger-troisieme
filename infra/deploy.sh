5ce726458db5d95fc8f34e7cb06894c33649f7a5
Etienne Weil <weil.etienne@hotmail.fr>
2024-02-22 11:03:57 +0100

    feat(deploy): new shell script + cleaning

---
+++ infra/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -a
+source .env
+set +a
+target='clever'
+git remote -vvv | grep $target | grep 'clever'
+
+if [ ! $? -eq 0 ]; then
+  echo "missing git remote $target"
+  echo "please add $target repo"
+  echo "-> git remote add $target $CLEVER_APP_ID"
+  exit 1;
+fi
+
+SSH_PRIV=~/.ssh/clevercloud-monstage
+if [ ! -f "$SSH_PRIV" ]; then
+  echo "missing private key to push, check kdbx for content"
+  exit 1;
+fi;
+
+git push $target master
